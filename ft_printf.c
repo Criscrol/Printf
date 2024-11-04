e7f29e26907c2773e1a592025588c4d7f40f4261
CRIS <crsandchess@gmail.com>
2024-11-04 11:26:29 +0100

    Add files via upload

--- ft_printf.c
+++ ft_printf.c
@@ -51,3 +51,20 @@ int	ft_printf(const char *format, ...)
 	va_end(args);
 	return (count);
 }
+
+/*
+#include <stdio.h>
+
+int main() {
+    int entero = 10;
+    float decimal = 5.5;
+    char caracter = 'A';
+    const char *cadena = "Hola, mundo!";
+
+    printf("Número entero: %d\n", entero);
+    printf("Número decimal: %.2f\n", decimal);
+    printf("Carácter: %c\n", caracter);
+    printf("Cadena: %s\n", cadena);
+
+    return 0;
+}*/
